11a683b2c8e77b2fc638fbe8852f1147a2620873
Krzysztof Milewski <milewski123@onet.pl>
2018-09-30 11:14:21 +0200

    Improved look on posts' pages

--- SandBox/App_Start/BundleConfig.cs
+++ SandBox/App_Start/BundleConfig.cs
@@ -7,10 +7,19 @@ namespace SandBox
         // Aby uzyskać więcej informacji o grupowaniu, odwiedź stronę https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootbox.min.js",
-                        "~/Scripts/underscore.min.js"));
+                        "~/Scripts/underscore.min.js",
+                        "~/Scripts/moment-with-locales.min.js",
+                        "~/Scripts/moment.min",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/respond.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                "~/Scripts/app/app.js"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,10 +28,6 @@ namespace SandBox
             // gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-sandstone.css",
---
+++ SandBox/Scripts/app/app.js
@@ -0,0 +1,46 @@
+﻿var PostsController = function () {
+
+    var loadPosts = function () {
+        $.getJSON("/api/posts", function (fetchedPosts) {
+            setPageTitle(fetchedPosts.length);
+
+            $.each(fetchedPosts, function (key, value) {
+                renderPost(value);
+                loadRelatedComments(value.id);              
+            });
+        });
+    };
+
+    var renderPost = function (postObject) {
+        var compiled = _.template($("#post-template").html());
+        $("#js-load-posts").append(compiled({ post: postObject }));
+    };
+
+    var renderComment = function (commentObject, postId) {
+        var compiledComment = _.template($("#comment-template").html());
+        
+        $("#" + postId + " .comment-section").append(compiledComment({ comment: commentObject }));
+    };
+
+    var setPageTitle = function (postsCount) {
+        if (postsCount > 0)
+            $("#page-title").html("Aktualności");
+        else
+            $("#page-title").html("Nie ma nic ciekawego do wyświetlenia");
+    };
+
+    var loadRelatedComments = function (postId) {
+        $.getJSON("/api/comments/" + postId, function (comments) {
+            if (comments.length > 0) {
+                $.each(comments, function (commentKey, commentValue) {
+                    renderComment(commentValue, postId);
+                });
+            }
+        });
+    };
+
+    return {
+        loadPosts: loadPosts
+    }
+
+}();
